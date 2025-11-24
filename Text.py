c1d69044b188a18e313c1a324000d900c97773bc
SaadFarooq1801 <saadfarooq1801@gmail.com>
2025-11-24 14:17:26 +0300

    Delete Text.py

--- Text.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from frame_sdk import Frame
-
-async def main():
-    async with Frame() as f:
-        await f.run_lua("frame.display.text('I am Saad', 50, 50, 100); frame.display.show()")
-
-asyncio.run(main())
