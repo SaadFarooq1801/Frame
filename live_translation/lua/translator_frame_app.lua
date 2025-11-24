24b8b8aa715ec9923c7b5b8d15ba4ae84e3439cd
SaadFarooq1801 <saadfarooq1801@gmail.com>
2025-11-24 15:12:07 +0300

    Update translator_frame_app.lua

--- live_translation/lua/translator_frame_app.lua
+++ live_translation/lua/translator_frame_app.lua
@@ -2,10 +2,27 @@ local data = require('data.min')
 
 USER_TEXT = 0x30  -- Message code for text messages
 
+-- Function to scroll text horizontally
+local function scroll_text(msg)
+    local width = frame.display.width
+    local text_len = string.len(msg)
+
+    -- Scroll text one character at a time
+    for i = 1, text_len do
+        frame.display.text(string.sub(msg, i), 1, 1)
+        frame.display.show()
+        frame.sleep(0.2)
+    end
+end
+
 -- Register parser
 data.parsers[USER_TEXT] = function(msg)
-    frame.display.text(msg, 1, 1)
-    frame.display.show()
+    if string.len(msg) <= 16 then  -- Adjust 16 for your screen width
+        frame.display.text(msg, 1, 1)
+        frame.display.show()
+    else
+        scroll_text(msg)
+    end
 end
 
 -- Main loop
